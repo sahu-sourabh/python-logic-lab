272c5f60ef0f773c4a54db717c268c32cc429a90
Sourabh Sahu <sourabhsahu.eng@gmail.com>
2026-05-14 19:15:21 +0530

    feat: add logic drill for swapping two numbers

---
+++ logic_drills/SwapTwoNumbers.py
@@ -0,0 +1,26 @@
+# Write a program to swap to numbers?
+
+def using_backup_variable(a, b):
+    backup_var = a # holds value of either a or b, only drawback we have used extra memory & processing power.
+    a = b
+    b = backup_var
+    print(f"Using backup variable {a} & {b}")
+
+def using_mathematical_operations(a, b):
+    # optimized solution without using extra memory & processing power.
+    a = a+b
+    b = a-b
+    a = a-b
+    print(f"Using mathematical operations {a} & {b}")
+
+def using_bitwise_operation(a,b):
+    # Without using mathatical operations.
+    # Most optimized because no coversion required from decimal to binary and vice versa.
+    a = a^b # a Ex-OR b
+    b = a^b # a Ex-OR b
+    a = a^b # a Ex-OR b
+    print(f"Using bitwise operations {a} & {b}")
+
+using_backup_variable(10, 20)
+using_mathematical_operations(10, 20)
+using_bitwise_operation(10,20)
